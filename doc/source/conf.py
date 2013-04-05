ff80c1404ec6443a950fe931c909c4666b15bffe
Matthias Runge <mrunge@redhat.com>
2013-04-05 10:16:19 +0200

    Don't access the net while building docs (Note this hasn't been submitted upstream)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -141,7 +141,6 @@ write_autodoc_index()
 # They can be extensions coming with Sphinx (named 'sphinx.ext.*')
 # or your custom ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
               'sphinx.ext.coverage',
               'sphinx.ext.pngmath',
