101bb0da4100a09ce803f683214a51abb9e9e8d9
Matthias Runge <mrunge@redhat.com>
2014-07-31 10:20:10 +0200

    RCUE navbar and login screen
    Conflicts:
    	openstack_dashboard/settings.py
    	tox.ini

--- openstack_dashboard/settings.py
+++ openstack_dashboard/settings.py
@@ -144,7 +144,7 @@ TEMPLATE_LOADERS = (
 
 TEMPLATE_DIRS = (
     os.path.join(ROOT_PATH, 'templates'),
-) 
+)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -211,7 +211,7 @@ COMPRESS_ENABLED = True
 COMPRESS_OUTPUT_DIR = 'dashboard'
 COMPRESS_CSS_HASHING_METHOD = 'hash'
 COMPRESS_PARSER = 'compressor.parser.HtmlParser'
-COMPRESS_OFFLINE = True
+COMPRESS_OFFLINE = False
 
 INSTALLED_APPS = [
     'openstack_dashboard',
@@ -227,15 +227,6 @@ INSTALLED_APPS = [
     'horizon',
     'openstack_auth',
 ]
-
-THEME_APP = 'openstack_dashboard_theme'
-
-try:
-    __import__(THEME_APP)
-    INSTALLED_APPS = (THEME_APP,) + INSTALLED_APPS
-except:
-    pass
-
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 AUTHENTICATION_BACKENDS = ('openstack_auth.backend.KeystoneBackend',)
